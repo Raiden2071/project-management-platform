902a8f7fc4338f93dea95e6790139df2b1c27862
slava-danilchak <danilchakv@gmail.com>
2025-03-24 14:20:51 +0200

    Hide Task field on the sidebar

--- src/widgets/sidebar/ui/NavigationItems.tsx
+++ src/widgets/sidebar/ui/NavigationItems.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import { List, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import HomeIcon from '@mui/icons-material/Home';
-import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
 import { useTranslation } from 'react-i18next';
 
 export const NavigationItems: React.FC = () => {
   const { t } = useTranslation();
+  const handleAddTask = () => {
+    // setSelectedTaskId(null);
+    // setTaskFormOpen(true);
+  };
 
   return (
     <List>
-      <ListItemButton>
+      <ListItemButton onClick={handleAddTask}>
         <ListItemIcon>
           <AddCircleIcon />
         </ListItemIcon>
@@ -24,12 +27,12 @@ export const NavigationItems: React.FC = () => {
         <ListItemText primary={t('nav.home')} />
       </ListItemButton>
       
-      <ListItemButton>
+      {/* <ListItemButton>
         <ListItemIcon>
           <FormatListBulletedIcon />
         </ListItemIcon>
         <ListItemText primary={t('nav.tasks')} />
-      </ListItemButton>
+      </ListItemButton> */}
     </List>
   );
 }; 
